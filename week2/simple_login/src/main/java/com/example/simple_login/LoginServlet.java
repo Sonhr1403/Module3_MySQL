07283516e38c0692151036a36925062c40b32f98
Sonhr1403 <110454300+Sonhr1403@users.noreply.github.com>
2022-10-27 10:22:08 +0700

    Add files via upload

---
+++ week2/simple_login/src/main/java/com/example/simple_login/LoginServlet.java
@@ -0,0 +1,37 @@
+package com.example.simple_login;
+
+import java.io.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+
+public class LoginServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        if ("admin".equals(username) && "admin".equals(password)) {
+            writer.println("<h1>Welcome " + username + " to website</h1>");
+        } else {
+            writer.println("<h1>Login Error</h1>");
+        }
+
+        writer.println("</html>");
+    }
+
+    public void destroy() {
+    }
+}
